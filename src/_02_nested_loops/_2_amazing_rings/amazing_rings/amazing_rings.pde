b8624cd1cbb40539c63f2999e9fdabb8b2ec5eb9
root <root@8a61f026446e>
2020-10-24 21:26:58 +0000

    working on for loop gauntlet

--- src/_02_nested_loops/_2_amazing_rings/amazing_rings/amazing_rings.pde
+++ src/_02_nested_loops/_2_amazing_rings/amazing_rings/amazing_rings.pde
@@ -1,6 +1,11 @@
-
+int width = 300;
+  int x = 200;
+  int x2 = 300;
+  int speed = 3;
+  int speed1 = 3;
+  int speed2 = -3;
 void setup() {
-  
+  size(600,600);
 }
 
 void draw() {
@@ -13,4 +18,37 @@ void draw() {
   Make the rings move past each other.
   When the rings hit the side of the sketch, make them reverse their direction.
   */
+background(148, 161, 237);
+   x=x+speed1;
+   x2=x2+speed2;
+   
+  for(int i = 0; i<30; i++) {
+    noFill();
+    ellipse(x, 250, width, width);
+    width -= 10;
+  }
+  
+  for(int i = 0; i<30; i++) {
+    ellipse(x2, 250, width, width);
+    width -= 10;
+  }
+  
+  width = 300;
+  if (x>=600) {
+    speed1 = speed1*-1;
+  }
+  else if(x<0) {
+    speed1 = speed1*-1;
+  }
+  
+  
+  
+  if( x2< 0) {
+    speed2 = speed2*-1;
+  }
+  else if(x>=600) {
+    speed2 = speed2*-1;
+  }
+    
+
 }
---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/for_loop_gauntlet.java
@@ -0,0 +1,59 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+public class for_loop_gauntlet {
+public static void main(String[] args) {
+	for (int i = 0; i < 101; i++) {
+		System.out.println(i);
+	}	
+	for (int i = 0; i < 101; i++) {
+		System.out.println(100-i);
+	}	
+	
+	for (int i = 1; i < 101; i++) {
+		if(i%2==0) {
+			System.out.println(i);
+		}
+	}
+	
+	for (int i = 0; i < 101; i++) {
+		if(i%2==1) {
+			System.out.println(i);
+		}
+	}
+	
+	for (int i = 1; i < 501; i++) {
+		if (i%2==0) {
+			System.out.println(i + " is even.");
+		}
+		else {
+			System.out.println(i + " is odd.");
+		}
+	}	
+	
+	for (int i = 0; i < 778; i++) {
+		if(i%7==0) {
+			System.out.println(i);
+		}
+	}	
+		
+	for (int i = 0; i < 17; i++) {
+		int year = 2004 + i;
+		System.out.println("In "+ year + " I was " + i + " years old.");
+	}	
+		
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			System.out.println(i + " " + j);
+			
+		}
+	}	
+	
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			System.out.println(j + " "+j+ " "+ j);
+		}
+	}
+		
+	}
+
+}
